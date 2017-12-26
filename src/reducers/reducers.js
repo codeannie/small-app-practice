a498a4dfe8ae1142594a56fa171938986fa891b9
Annie Chiang <devbear22@gmail.com>
2017-12-26 12:16:31 -0800

    fix how state was returned & added notes

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -2,20 +2,27 @@ import { ADD_NUMBER } from "../actions/actionCreator";
 
 // reducers
 
+// initial state should outline what state should look like
+// if have multiple properties, easier to reference or set an empty {}
+
+// const initialState = {}
+// if set initial state to {} so when other components reference it as a prop
+// those components will errors initially if there is no empty {} to start with
+// otherwise need do null checks everywhere 
 const initialState = {
-  numbers: [ 5, 7, 8]
+  numbers: [ 5, 7, 8 ]
 };
 
-const numberReducer = (initialState, action) => {
+const numberReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_NUMBER: 
     console.log('adding number...');
-      return [
-        ...initialState,
-        action.payload.number
-      ];
+      return {
+        ...state, // { numbers: [5, 7, 8] } = original state
+        numbers: [...state.numbers, action.payload.number] //this overrides the properties in the state 
+      };
     default: 
-      return initialState;
+      return state;
   }
 }
 
